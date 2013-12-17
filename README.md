95cc080ce5056a5f9cdb7fe4507be6454189469a
Claud Wang <mrclaudwang@gmail.com>
2013-12-16 18:38:57 -0800

    refactor the code to be cleaner

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 WorkflowScheduling-Claud
 ========================
 
-
-This readme will tell you something about this project.
+NASA’s High-End Computing Capability (HECC) project is designed to accelerate and enhance scientific discovery and aeronautics research. The mission of this project is to develop a technique that bridges the gap between scientific workflow design tool and NASA’s Supercomputer Center, so that scientists can run their big data experiments at NASA’s Supercomputer Center and Amazon cloud without leaving their design environment. In this technical report, we describe our proposed architecture and the implementation of a working prototype to demonstrate the feasibility of our solution.
--- src/AmazonPlugin/scheduler.py
+++ src/AmazonPlugin/scheduler.py
@@ -56,9 +56,10 @@ class Scheduler:
         self.graph = self.get_graph()
 
     def read_workflow(self):
-        jsonfile=open('/Users/claudwang/.vistrails/userpackages/AMAZONPlugin/workflow.json')
+        jsonfile=open('/Users/claudwang/.vistrails/userpackages/AMAZONPlugin/workflow.json') 
+        #Change the address if you use it at your local
+        # you can find the json file format in the "test" folder
         workflow = json.load(jsonfile)
-        #print_workflow(workflow)
         jsonfile.close()
         return workflow
 
@@ -72,106 +73,25 @@ class Scheduler:
 
     def read_workload(self):
         jsonfile=open('/Users/claudwang/.vistrails/userpackages/AMAZONPlugin/workload.json')
+        #Change the address if you use it at your local
+        # you can find the json file format in the "test" folder
         workload = json.load(jsonfile)
-        #print_workload(workload)
         jsonfile.close()
         return workload
 
     def read_nodeinfo(self):
         jsonfile=open('/Users/claudwang/.vistrails/userpackages/AMAZONPlugin/nodeinfo.json')
+        #Change the address if you use it at your local
+        # you can find the json file format in the "test" folder
         nodeinfo = json.load(jsonfile)
-        #print_nodeinfo(nodeinfo)
         jsonfile.close()
         return nodeinfo
 
     def get_overall_deadline(self):
         deadline = 1000
+        # test data
         return deadline
-
-    def assign_deadline(self):
-        overall_deadline = self.get_overall_deadline()
-        workloads = self.workloads
-        work_deadline = {}
-        total_workload = 0
-        for work,workload in workloads.iteritems():
-            total_workload += workload
-        for work,workload in workloads.iteritems():
-            work_deadline.setdefault(work,overall_deadline*workload/total_workload)
-        return work_deadline
-
-    def get_nodes_info(self):
-        nodes_info = { "x":(10,150),"y":(30,400),"z":(20,300) }
-        return nodes_info
-
-    def check_resource(self):
-        resource_list = { "x":3,"y":4,"z":2 }
-        return resource_list
-   
-    def partition(self):
-        uow = {}
-        dependency = {}
-        uow.setdefault(self.wf.keys()[0],[]) # add start point
-        for node,point_to in self.wf.iteritems():
-            if len(point_to)!=0:
-                for tnode in point_to:
-                    dependency.setdefault(tnode,[])
-                    dependency[tnode].append(node)
-        for tnode,point_to in dependency.iteritems():
-            if len(point_to)>1:
-                uow.setdefault(tnode,[])   # sync node
-        for tnode in uow:
-            if dependency.has_key(tnode):
-                dependency.pop(tnode)
-        for tnode,point_to in dependency.iteritems():
-            if dependency.has_key(point_to[0]):
-                uow.setdefault(tnode,[])
-                uow[tnode].append(point_to[0])
-                uow.setdefault(point_to[0],-1)
-            elif not uow.has_key(tnode):
-                uow.setdefault(tnode,[])
-        for tnode in uow:
-            if dependency.has_key(tnode):
-                dependency.pop(tnode)
-        uowl = []
-        for node,value in uow.iteritems():
-            if value != -1:
-                if len(value)==0:
-                    uowl.append(node)
-                else:
-                    t = value
-                    t.append(node)
-                    uowl.append(t)
-        return uowl
-
-    def generate_node(self,updated_list,nodes_info):
-        #for node,info in nodes_info.iteritems():
-        #sorted(nodes_info.iteritems(),key = lambda x:x[1])[0]
-        if int(random.random()*100)%4 == 0:
-            return "Sandy_Bridge"
-        elif int(random.random()*100)%4 == 0:
-            return "Westmere"
-        elif int(random.random()*100)%4 == 0:
-            return "Nehalem"
-        else:
-            return "Harpertown"
-
-    def old_schedule(self):
-        work_deadline = self.assign_deadline()
-        independent_task,sequence_task = self.partition()
-        independent_task_node = self.assign_node(independent_task,work_deadline)
-        resource_list = self.check_resource()
-        nodes_info = self.get_nodes_info()
-        deadline = self.get_overall_deadline()
-        plan = {}
-        updated_list = resource_list
-        for work in uowl:
-            if type(work)==list:
-                for job in work:
-                    plan.setdefault(job,self.assign_node(updated_list,nodes_info))
-            else:
-                plan.setdefault(work,self.assign_node(updated_list,nodes_info))
-        return plan
-
+    
     def assign_node(self, task_list, work_deadline):
         workloads = self.workloads
         task_bottomline = {}
@@ -198,12 +118,7 @@ class Scheduler:
             print "%s ---> %s"%(task,node)
             print "Esimatied Cost: $%d"%(nodeinfo[node][1])
         return 0
-
-    def get_cost(self,san,wes,neh,har):
-        # SBU charged = Wall_Clock_Hours_Used * Number of MAUs * SBU Rate
-        cost = 100*san*1.82+100*wes*1+100*neh*0.8+100*neh*0.45
-        return cost
-
+    
     def get_computing_power(self):
         nodeinfo = self.nodeinfo
         # u'Harpertown': [u'3', 600, 100]
@@ -308,7 +223,6 @@ class Scheduler:
 
 def main():
     scheduler = Scheduler()
-    work_deadline = scheduler.assign_deadline()
     node_computing = scheduler.schedule() 
     scheduler.print_schedule(node_computing)
 
